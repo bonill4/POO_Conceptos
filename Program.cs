b1efd0f1f6972a8ce0a82656c6a44195a27b1ba6
bonill4 <williamsbonill11@gmail.com>
2024-01-08 18:35:49 -0600

    Commit 1: Conceptos de POO

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Importacion de metodos static
+No es muy recomendable por confuciones
+
+using static System.Math;
+using static System.Console;
+*/
+
+namespace POO_Conceptos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //realizarTarea();
+
+            //CLASE ANONIMAS
+            //Si las clase anonima tiene la misma cantidad de propiedades y del mismo tipo 
+            //el copilador asume que es de la misma clase anonima
+            var miVariable = new {Nombre = "Williams", Edad = 23};
+            var miOtraVariable = new { Nombre = "Ulises", Edad = 25 };
+
+            miVariable = miOtraVariable;
+
+            Console.WriteLine(miVariable.Nombre+" "+miVariable.Edad);
+
+            
+        }
+
+        static void realizarTarea()
+        {
+            Punto origen = new Punto();
+            Punto destino = new Punto(128, 80);
+
+            double distancia = origen.DistanciaHasta(destino);
+
+            Console.WriteLine($"La distancia de los puntos es de: {distancia}");
+
+            Console.WriteLine($"Numero de objetos creador {Punto.getContadorDeObjetos()}");
+        }
+    }
+}
---
+++ Punto.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Conceptos
+{
+    class Punto
+    {
+        private int x, y;
+
+        private static int contadorDeObjeto = 0; 
+
+        public Punto()
+        {
+            this.x = 0;
+            this.y = 0;
+
+            contadorDeObjeto++;
+        }
+        public Punto(int x, int y) 
+        {
+            this.x = x;
+            this.y = y;
+
+            contadorDeObjeto++;
+        }
+
+        public double DistanciaHasta(Punto otroPunto)
+        {
+            int xDif = this.x - otroPunto.x;
+
+            int yDif = this.y - otroPunto.y;
+
+            double distanciaPunto = Math.Sqrt(Math.Pow(xDif, 2)+Math.Pow(yDif, 2));
+
+            return distanciaPunto;
+        }
+
+        public static int getContadorDeObjetos() => contadorDeObjeto;
+    }
+}
